99241b358a271f069a690333fbe93e22ceac23a6
赵庆 <805322803@qq.com>
2025-12-03 10:01:52 +0800

    测试 错误输入

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <main>{{ info }} -- {{ time }}</main>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+const info = ref<string>('我想知道你在哪，你现在在干嘛')
+const time = ref<string>('')
+
+const changeTime = () => {
+  setInterval(() => {
+    time.value = new Date().toDateString()
+  }, 1000)
+}
+
+onMounted(() => {
+  changeTime()
+})
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -10,6 +27,14 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: center;
   }
+}
+
+main {
+  font-weight: bold;
+  font-size: 30px;
+  text-shadow: 0 5px 4px rgba(0, 0, 0, 0.5);
 }
 </style>
